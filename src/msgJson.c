16f8fdbc8e76961156dc6a021078724a813a4720
zzpp <2036459752@qq.com>
2021-10-16 11:15:28 +0800

    json

--- src/msgJson.c
+++ src/msgJson.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 #include "../inc/cjson.h"
 
-
+void jsonConnect(char *temp)                                       
+{                                                                 
+ 	cJSON *root_json = cJSON_CreateObject();                                         
+ 	cJSON *PubData = cJSON_CreateObject();                                         
+ 	cJSON_AddNumberToObject(root_json,"protocol",1);                                         
+ 	cJSON_AddNumberToObject(root_json,"code",0);                                         
+ 	cJSON_AddStringToObject(PubData,"orderId","single");                                         
+ 	cJSON_AddItemToObject(root_json,"data",PubData);                                         
+ 	char *buff=cJSON_PrintUnformatted(root_json);
+ 	memcpy(temp,buff,strlen(buff)); 
+ 	cJSON_Delete(root_json);                                         
+}	              
 
